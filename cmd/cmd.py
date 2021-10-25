dfbd87893bc68d541a84132d8685c134dc89585e
Maelus999-QuartzStudio <93131844+Maelus999-QuartzStudio@users.noreply.github.com>
2021-10-25 15:59:47 +0200

    Update cmd.py

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -8,3 +8,16 @@ def cmd_version():
 
 def cmd_no_shutdown():
     os.system("shutdown /a")
+
+def cmd_new_txtFiles():
+    name_of_file = input("quel nom voulez vous pour le fichier? : ")
+    myFile = open(name_of_file+".txt", "w+")
+    texte_file = input("que voulez vous marquer dedans? : ")
+    myFile.write(texte_file)
+
+    myFile.close()
+
+def cmd_new_file():
+    fichier_name = input("Nom du Fichier : ")
+    os.system("mkdir " + fichier_name)
+    print("le fichier a était installer dans vos document")
