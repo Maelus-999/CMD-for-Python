ba710a877856fa96efe671916f62b82e9128966e
Maelus999-QuartzStudio <93131844+Maelus999-QuartzStudio@users.noreply.github.com>
2021-10-25 16:02:35 +0200

    Create demo_cmd.py

---
+++ cmd/demo_cmd.py
@@ -0,0 +1,8 @@
+import cmd
+
+input("Voici la commande qui indique la version de cmd, usage:cmd.cmd_version()")
+cmd.cmd_version()
+input("Voici la commande qui crée un fichier texte, usage:cmd.cmd_new_txtFiles()")
+cmd.cmd_new_txtFiles()
+input("Voici la commande qui crée un dossier, usage:cmd.cmd_new_File()")
+cmd.cmd_new_file()
