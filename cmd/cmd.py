9086a43947cf3cba3c2973eaa935d66c9c549b3a
Maelus999-QuartzStudio <93131844+Maelus999-QuartzStudio@users.noreply.github.com>
2021-10-26 10:01:14 +0200

    Update cmd.py
    Ajout de nouvelle commande ("voir demo_cmd.py") ainsi qu'une optimisation au niveaux du code

--- cmd/cmd.py
+++ cmd/cmd.py
@@ -1,15 +1,13 @@
 import config.version
-from config import *
-import os, sys
+import os
 
+def version():
+    print('Voici la version de cmd:' + config.version.version)
 
-def cmd_version():
-    print("the version of cmd is: "+ config.version.version)
-
-def cmd_no_shutdown():
+def no_shutdown():
     os.system("shutdown /a")
 
-def cmd_new_txtFiles():
+def new_txtFiles():
     name_of_file = input("quel nom voulez vous pour le fichier texte? : ")
     myFile = open(name_of_file+".txt", "w+")
     texte_file = input("que voulez vous marquer dedans? : ")
@@ -17,7 +15,21 @@ def cmd_new_txtFiles():
 
     myFile.close()
 
-def cmd_new_file():
+def new_file():
     fichier_name = input("Nom du Fichier : ")
     os.system("mkdir " + fichier_name)
-    print("le fichier a était installer dans vos document")
+    print('le fichier a était installer dans vos document')
+
+
+def pause():
+    os.system("pause")
+
+def echo():
+    echo = input("que voulez vous ecrire? : ")
+    os.system("echo "+echo)
+
+def win():
+    os.system("ver")
+
+def cmd():
+    os.system("start")
